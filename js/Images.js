5af702390f11b6058d549d52ee0bb8cd825e512d
Aki <71226118+Aki-Gonzalez@users.noreply.github.com>
2021-04-13 04:05:23 +0200

    webpage no longer placeholder

---
+++ js/Images.js
@@ -0,0 +1,6 @@
+function SwapSource(first, second) {
+    document.get
+    var temp = document.getElementById(first).src;
+    document.getElementById(first).src = document.getElementById(second).src;
+    document.getElementById(second).src = temp;
+}
